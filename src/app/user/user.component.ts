6fa4da2432670b14a64633de6bf7a696c6f69ab8
esha <acharya.e@northeastern.edu>
2024-07-04 17:57:09 -0400

    output conditional content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -9,18 +21,15 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
 
-  //@Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(){
-    return 'assets/users/'+ this.avatar;
+    return 'assets/users/'+ this.user.avatar;
   }
 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
